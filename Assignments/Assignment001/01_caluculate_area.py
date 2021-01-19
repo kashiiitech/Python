59deb387f8307e8336e9cd6601083c9e63b8fe19
Kashif Ali <74322848+kashiiitech@users.noreply.github.com>
2021-01-19 13:31:19 +0500

    Add files via upload

---
+++ Assignments/Assignment001/01_caluculate_area.py
@@ -0,0 +1,8 @@
+""" function with the name “calculateArea” that takes in two inputs (width and
+length) and return the area. """ 
+
+
+
+def calculateArea(width,length):
+    area = width*length
+    return area
