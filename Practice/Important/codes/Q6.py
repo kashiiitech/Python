8f2c953fe63a299196d75d2bedecdbd3f46a4862
Kashif Ali <74322848+kashiiitech@users.noreply.github.com>
2021-01-20 16:42:48 +0500

    Update Q6.py

--- Practice/Important/codes/Q6.py
+++ Practice/Important/codes/Q6.py
@@ -2,10 +2,10 @@
 
 
 
-def sum_integer(x):
-    y = str(x)
-    summ = 0
-    for i in y:
-        i = int(i)
-        summ += i
-    return summ
+def sum_integer(x):  
+    y = str(x)        # Here we change type of number from integer to string
+    summ = 0          
+    for i in y:      # this loop will take every digit from the string in every iteration
+        i = int(i)   # the type of i is string so here we change into the integer
+        summ += i  # here the variable named summ has initially zero but in every iterartion the i will be added to the summ and tha assign into the summ .
+    return summ    # in last the sum of all digits is in the variable summ here we return it to the function from where it is called .
