553e090b7fe78222e6115491caeb4bcf862b3243
Kashi <you@example.com>
2021-01-21 10:48:46 +0500

    initial commit

---
+++ Practice/reverse_number.py
@@ -0,0 +1,11 @@
+# Reverse a given number and return true if it is the same as the original number
+
+def reverse_num(num):
+    y = str(num)
+    k = ""
+    for l in range(len(y)-1,-1,-1):
+        k = k+y[l]
+        if k == y:
+            return True
+    else:
+        return False
