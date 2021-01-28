83b4773e6790d5bbf7f872066f0cf9b1e9438bab
Kashif Ali <74322848+kashiiitech@users.noreply.github.com>
2021-01-28 12:58:58 +0500

    code_for_get_companies_names.py

--- Assignments/Assignment 005/Code/code_for_company_names.py
+++ Assignments/Assignment 005/Code/code_for_company_names.py
@@ -1,6 +1,11 @@
+"""function with the name “get_companies_names”. The function will be
+given a list of dictionaries following the above specifications. The function should
+return a list containing “Company Name” from each dictionary in the given list."""
+
+
 def get_companies_names(company):
-    khan = []
+    list_of_company_names = []
     for i in company:
         k = i['Company Name']
-        khan.append(k)
-    return khan
+        list_of_company_names.append(k)
+    return list_of_company_names
