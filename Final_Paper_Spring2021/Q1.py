bf01fdd9ea2ab2e6e1d84234b03468b3e9c6278b
Kashif Ali <kashiiitech@gmail.com>
2021-08-18 01:02:30 +0500

    Delete Q1.py

--- Final_Paper_Spring2021/Q1.py
+++
@@ -1,6 +0,0 @@
-# Question no 1
-
-def total_overs(num):  # it will take the number of total balls as parameter
-    over = num/6       # it will calculate the overs and assign to the variable over
-    return over        # it will return the over to the function call
-    
